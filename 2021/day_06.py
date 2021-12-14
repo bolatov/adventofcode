7e658bdce6a2015c0650d94af58684097d39f9c3
Almer Bolatov <almer.bolatov@deliveryhero.com>
2021-12-14 11:29:21 +0100

    Day 6
    Part 2

--- 2021/day_06.py
+++ 2021/day_06.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-from collections import deque
+from collections import deque, Counter
 import sys
 
 
@@ -10,9 +10,8 @@ def _read(input_file="input.txt"):
         return [int(i) for i in line.split(",")]
 
 
-def part_1(input_file):
+def part_1(input_file, days=80):
     state = deque(_read(input_file))
-    days = 80
     for day in range(days):
         curlen = len(state)
         for _ in range(curlen):
@@ -24,7 +23,27 @@ def part_1(input_file):
     return len(state)
 
 
+def part_2(input_file, days=80):
+    numbers = _read(input_file)
+    mp = [0 for _ in range(10)]
+    for num in numbers:
+        mp[num] += 1
+    for day in range(days):
+        new_mp = [0 for _ in range(10)]
+        for num, count in enumerate(mp):
+            if num == 0:
+                new_mp[6] += count
+                new_mp[8] += count
+            else:
+                new_mp[num-1] += count
+        mp = new_mp
+    return sum(mp)
+
+
 if __name__ == "__main__":
     input_file = sys.argv[1]
-    ans = part_1(input_file)
-    print("Part 1:", ans)
+    days = int(sys.argv[2])
+    #ans = part_1(input_file, days)
+    #print("Part 1:", ans)
+    ans = part_2(input_file, days)
+    print("Part 2:", ans)
