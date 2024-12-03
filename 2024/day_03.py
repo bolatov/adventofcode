c2c87b83bbddb00360ef04678a2e3b24fde001b9
Almer Bolatov <almer.bolatov@gmail.com>
2024-12-03 18:12:14 +0100

    2024: Day 3 - Part 2

--- 2024/day_03.py
+++ 2024/day_03.py
@@ -1,18 +1,21 @@
 #!/usr/local/bin/python3
 import re
+import sys
 
 def read_input_as_string(filename="input.txt"):
-    res = ""
+    res = "" 
     with open(filename) as f:
         lines = f.readlines()
         for line in lines:
             res += line
-    return res
+        return res
 
 
 MUL_OPEN = "mul("
 COMMA = ","
 CLOSE = ")"
+DO = "do()"
+DONT = "don't()"
 
 
 def part1(s):
@@ -44,7 +47,7 @@ def part1(s):
         else:
             i += 1
 
-    print("Part 1: ", result)
+    print("Part 1:", result)
     return result
 
 
@@ -58,17 +61,65 @@ def part1_with_regex(s):
     return res
 
 
-def part2(matrix):
+def part2_with_regex(s):
+    matches = re.findall(r"do\(\)|don't\(\)|mul\(\d+,\d+\)", s, re.DOTALL)
     result = 0
-    for arr in matrix:
-        if is_safe_with_toleration(arr):
-            #print("Part 2:", arr)
-            result += 1
+    enabled = True
+    for match in matches:
+        if DO == match:
+            enabled = True
+            #print("Enabled")
+            continue
+        elif DONT == match:
+            enabled = False
+            #print("Disabled")
+            continue
+        if not enabled:
+            continue
+        x, y = match[len(MUL_OPEN):len(match)-1].split(",")
+        #print(f"\tMultiply {x} * {y}")
+        result += int(x) * int(y)
+    print("Part 2 (with regex):", result)
+    return result
+    
+
+def part2(s):
+    result = 0
+    i, n = 0, len(s)
+    enabled = True
+    while i < n:
+        if DO == s[i:i+len(DO)]:
+            enabled = True
+            i += len(DO)
+        elif DONT == s[i:i+len(DONT)]:
+            enabled = False
+            i += len(DONT)
+        elif MUL_OPEN == s[i:i+len(MUL_OPEN)]:
+            i += len(MUL_OPEN)
+            x = ""
+            while i < n and ("0" <= s[i] <= "9"):
+                x += s[i]
+                i += 1
+            if s[i] == ",":
+                i += 1
+                y = ""
+                while i < n and ("0" <= s[i] <= "9"):
+                    y += s[i]
+                    i += 1
+                if s[i:i+len(CLOSE)] == CLOSE:
+                    i += len(CLOSE)
+                    if enabled:
+                        result += int(x) * int(y)
+        else:
+            i += 1
     print("Part 2: ", result)
     return result
 
+
 if __name__ == "__main__":
-    s = read_input_as_string()
+    filename = "input.txt" if len(sys.argv) < 2 else sys.argv[1]
+    s = read_input_as_string(filename)
     part1(s)
     part1_with_regex(s)
-    #part2(matrix)
+    part2(s)
+    part2_with_regex(s)
