cf0dff991043cea00b5ae9121dcd92d55a99407f
Almer Bolatov <almer.bolatov@gmail.com>
2024-12-05 11:00:48 +0100

    2024: Day 5 - Part 2

--- 2024/day_05.py
+++ 2024/day_05.py
@@ -2,6 +2,7 @@
 import re
 import sys
 
+
 def read_input(filename="input.txt"):
     rules = {} # int -> int[]
     matrix = []  # int[][]
@@ -30,38 +31,67 @@ def read_input(filename="input.txt"):
         return rules, matrix
 
 
-#def get_correct_orders(rules, matrix):
-def part1(rules, matrix):
-    result = 0
+def in_right_order(rules, mp):
+    # check if rules are respected
+    for rule_key, rule_nums in rules.items():
+        if rule_key not in mp:
+            continue
+        rule_key_positions = mp[rule_key]
+        for left_pos in rule_key_positions:
+            for rule_num in rule_nums:
+                if rule_num not in mp:
+                    continue
+                for right_pos in mp[rule_num]:
+                    if left_pos > right_pos:
+                        return [left_pos, right_pos]
+    return []
+
+
+def build_mp(arr):
+    mp = {}
+    for i in range(len(arr)):
+        pos = mp.get(arr[i], [])
+        pos.append(i)
+        mp[arr[i]] = pos
+    return mp
+
+
+def get_correct_orders(rules, matrix):
+    result = []
     for matrix_row in range(len(matrix)):
         arr = matrix[matrix_row]
-        in_right_order = True
-        mp = {}
-        for i in range(len(arr)):
-            pos = mp.get(arr[i], [])
-            pos.append(i)
-            mp[arr[i]] = pos
-        #print(mp)
-        # check if rules are respected
-        for rule_key, rule_nums in rules.items():
-            if rule_key not in mp:
-                continue
-            rule_key_positions = mp[rule_key]
-            for left_pos in rule_key_positions:
-                for rule_num in rule_nums:
-                    if rule_num not in mp:
-                        continue
-                    for right_pos in mp[rule_num]:
-                        if left_pos > right_pos:
-                            in_right_order = False
-        if in_right_order:
-            result += arr[len(arr)//2]
+        mp = build_mp(arr)
+        if in_right_order(rules, mp) == []:
+            result.append(matrix_row)
+    return result
+
+
+def part1(rules, matrix):
+    result = 0
+    for irow in get_correct_orders(rules, matrix):
+        result += matrix[irow][len(matrix[irow])//2]
     print("Part 1:", result)
     return result
 
 
-def part2(s):
+def order(rules, arr):
+    mp = build_mp(arr)
+    res = in_right_order(rules, mp)
+    if res == []:
+        return arr
+    ipos, jpos = res
+    arr[jpos], arr[ipos] = arr[ipos], arr[jpos]
+    return order(rules, arr)
+
+
+def part2(rules, matrix):
     result = 0
+    correct_orders = set(get_correct_orders(rules, matrix))
+    for irow in range(len(matrix)):
+        if irow in correct_orders:
+            continue
+        ordered = order(rules, matrix[irow])
+        result += ordered[len(ordered)//2]
     print("Part 2: ", result)
     return result
 
@@ -70,4 +100,4 @@ if __name__ == "__main__":
     filename = "input.txt" if len(sys.argv) < 2 else sys.argv[1]
     rules, matrix = read_input(filename)
     part1(rules, matrix)
-    #part2(s)
+    part2(rules, matrix)
