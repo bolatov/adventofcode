6431f1ff06b185c25b8ef43a09b2f5954274d72e
Almer Bolatov <almer.bolatov@gmail.com>
2024-12-09 06:43:28 +0100

    2024: Day 8 - Part 2

--- 2024/day_08.py
+++ 2024/day_08.py
@@ -55,9 +55,64 @@ def part1(matrix):
     return result
 
 
+def calculate_antinodes_upd(position_a, position_b, rows, cols):
+    nodes = {position_a, position_b} # set() # position
+    row_a, col_a = position_a
+    row_b, col_b = position_b
+    row_delta = row_b - row_a
+    col_delta = col_b - col_a
+
+    row, col = position_a
+    count = 0
+    while is_between(0, rows, row - row_delta) and is_between(0, cols, col - col_delta):
+        count += 1
+        row -= row_delta
+        col -= col_delta
+        nodes.add((row, col))
+    #if count > 1:
+    #    nodes.add(position_b)
+
+    row, col = position_b
+    count = 0
+    while is_between(0, rows, row + row_delta) and is_between(0, cols, col + col_delta):
+        count += 1
+        row += row_delta
+        col += col_delta
+        nodes.add((row, col))
+    #if count > 1:
+    #    nodes.add(position_a)
+    return nodes
+
+
+def print_matrix(matrix, antinodes):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            pos = (row, col)
+            if pos in antinodes: # and matrix[row][col] == ".":
+                    print("#", end="")
+            else:
+                print(matrix[row][col], end="")
+            print(" ", end="")
+        print()
+    print()
+
+
 def part2(matrix):
-    result = 0
-    print(f"Part 1: {result}")
+    result = set() #part1(matrix)
+    freq_pos = get_frequency_positions(matrix)
+    rows, cols = len(matrix), len(matrix[0])
+    for positions in freq_pos.values():
+        for i in range(len(positions)):
+            pos_a = positions[i]
+            for j in range(i+1, len(positions)):
+                pos_b = positions[j]
+                ab = calculate_antinodes_upd(pos_a, pos_b, rows, cols)
+                result = result.union(ab)
+    #for positions in freq_pos.values():
+    #    if len(positions) == 1:
+    #        result.discard(positions[0])
+    #print_matrix(matrix, result)
+    print(f"Part 2: {len(result)}")
     return result
 
 
@@ -65,4 +120,4 @@ if __name__ == "__main__":
     filename = "input.txt" if len(sys.argv) < 2 else sys.argv[1]
     matrix = read_input(filename)
     part1(matrix)
-    #part2(matrix)
+    part2(matrix)
