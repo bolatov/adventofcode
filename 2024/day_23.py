57151fa644865ad001e3098d89d2c6f1f4689ced
Almer Bolatov <almer.bolatov@gmail.com>
2025-01-11 01:45:10 +0100

    2024: Day 23 - Part 2

--- 2024/day_23.py
+++ 2024/day_23.py
@@ -14,15 +14,7 @@ def read_input(filename="input.txt"):
     return arr
 
 
-def part1(pairs):
-    g = {}
-    for a, b in pairs:
-        if a not in g:
-            g[a] = []
-        if b not in g:
-            g[b] = []
-        g[a].append(b)
-        g[b].append(a)
+def part1(g):
     result = set()
     for a in g.keys():
         for b in g[a]:
@@ -32,12 +24,47 @@ def part1(pairs):
     return len(result)
 
 
-def part2(secrets):
-    result = 0
-    return result
+def part2(g):
+    result = None
+    for v in g.keys():
+        print(f"{v}")
+        vcopy = g[v].copy()
+        vcopy.add(v)
+        local_result = None
+        for u in g[v]:
+            ucopy = g[u].copy()
+            ucopy.add(u)
+            print(f"\t {vcopy} {ucopy}")
+            intersection = vcopy & ucopy
+            all_connected = True
+            for k in intersection:
+                kcopy = g[k].copy()
+                kcopy.add(k)
+                if not (intersection < kcopy):
+                    all_connected = False
+                    break
+            if all_connected and (result is None or len(intersection) > len(result)):
+                result = intersection
+    if result is None:
+        return None
+    return ",".join(sorted(result))
+
+
+def convert_to_graph(pairs):
+    g = {}
+    for a, b in pairs:
+        if a not in g:
+            g[a] = set()
+        if b not in g:
+            g[b] = set()
+        g[a].add(b)
+        g[b].add(a)
+    return g
+
 
 if __name__ == "__main__":
     filename = "input.txt" if len(sys.argv) < 2 else sys.argv[1]
     arr = read_input(filename)
-    print("Part 1: ", part1(arr))
-    #print("Part 2: ", part2(arr, 2000))
+    graph = convert_to_graph(arr)
+    print("Part 1: ", part1(graph))
+    print("Part 2: ", part2(graph))
